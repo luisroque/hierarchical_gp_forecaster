4f767e93e1f0ca2d5b7ce6834b07e1eaaab6c456
mach1ne <luis_roque@live.com>
2022-02-04 14:30:52 +0000

    updated version of unit test for police dataset

--- gpforecaster/tests/test_model_results_police.py
+++ gpforecaster/tests/test_model_results_police.py
@@ -7,7 +7,7 @@ import shutil
 class TestModel(unittest.TestCase):
 
     def setUp(self):
-        self.data = tsag.preprocessing.PreprocessDatasets('police', top=100).apply_preprocess()
+        self.data = tsag.preprocessing.PreprocessDatasets('police', top=10).apply_preprocess()
         self.n = self.data['predict']['n']
         self.s = self.data['train']['s']
         shutil.rmtree("./data/original_datasets")
@@ -24,5 +24,4 @@ class TestModel(unittest.TestCase):
     def test_predict_shape(self):
         model, like = self.gpf.train(n_iterations=10)
         mean, lower, upper = self.gpf.predict(model, like)
-        print(mean.shape)
         self.assertTrue(mean.shape == (1, self.n, self.s))
